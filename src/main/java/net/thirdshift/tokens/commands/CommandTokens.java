cd32761e1cf3c12f57280d862cf829cf3be57322
TheCoolestPaul <Honda15251526>
2020-02-04 15:23:39 -0600

    Added /set command

--- src/main/java/net/thirdshift/tokens/commands/CommandTokens.java
+++ src/main/java/net/thirdshift/tokens/commands/CommandTokens.java
@@ -26,15 +26,54 @@ public class CommandTokens implements CommandExecutor {
         }
         if(args[0].equalsIgnoreCase("add")) {
             if (commandSender instanceof Player) {
-                if(commandSender.hasPermission("tokens.add")) {
-                    Player target = Bukkit.getPlayer(args[1]);
+                if (commandSender.hasPermission("tokens.add")) {
+                    if (args.length == 3) {
+                        Player target = Bukkit.getPlayer(args[1]);
+                        if(target!=null) {
+                            int num = Integer.parseInt(args[2]);
+                            plugin.handler.addTokens(target, num);
+                        }else{
+                            commandSender.sendMessage(ChatColor.RED+"Couldn't find player " + args[1] + ". did you spell their username correct?");
+                        }
+                    } else {
+                        commandSender.sendMessage(ChatColor.RED + "Invalid command use.");
+                        commandSender.sendMessage(ChatColor.GRAY + "Command usage: /tokens add <player name> <tokens amount>");
+                    }
+                }
+            } else {
+                Player target = Bukkit.getPlayer(args[1]);
+                if(target!=null) {
                     int num = Integer.parseInt(args[2]);
                     plugin.handler.addTokens(target, num);
+                }else{
+                    commandSender.sendMessage(ChatColor.RED+"Couldn't find player " + args[1] + ". did you spell their username correct?");
+                }
+            }
+            return true;
+        }else if(args[0].equalsIgnoreCase("set")){
+            if(args.length==3){
+                if(commandSender instanceof Player){
+                    if(commandSender.hasPermission("tokens.set")){
+                        Player target = Bukkit.getPlayer(args[1]);
+                        if(target!=null){
+                            plugin.handler.setTokens(target, Integer.parseInt(args[2]));
+                            commandSender.sendMessage("Set "+target.getName()+"'s tokens to "+args[2]);
+                        }else{
+                            commandSender.sendMessage(ChatColor.RED+"Couldn't find player " + args[1] + ". did you spell their username correct?");
+                        }
+                    }
+                }else{
+                    Player target = Bukkit.getPlayer(args[1]);
+                    if(target!=null){
+                        plugin.handler.setTokens(target, Integer.parseInt(args[2]));
+                        commandSender.sendMessage("Set "+target.getName()+"'s tokens to "+args[2]);
+                    }else{
+                        commandSender.sendMessage(ChatColor.RED+"Couldn't find player " + args[1] + ". did you spell their username correct?");
+                    }
                 }
             }else{
-                Player target = Bukkit.getPlayer(args[1]);
-                int num = Integer.parseInt(args[2]);
-                plugin.handler.addTokens(target, num);
+                commandSender.sendMessage(ChatColor.RED + "Invalid command use.");
+                commandSender.sendMessage(ChatColor.GRAY + "Command usage: /tokens set <player name> <tokens amount>");
             }
             return true;
         }else if(args[0].equalsIgnoreCase("give")) {
@@ -54,14 +93,14 @@ public class CommandTokens implements CommandExecutor {
                             target.sendMessage("You received " + ChatColor.GOLD + "" + args[2] + "" + ChatColor.WHITE + " token(s) from " + ChatColor.GREEN + "" + commandSender.getName());
                         } else {
                             commandSender.sendMessage(ChatColor.RED+"Couldn't find player " + args[1] + ". did you spell their username correct?");
-                            commandSender.sendMessage(ChatColor.RED+"Try /tokens give <UserName> <Amount>");
+                            commandSender.sendMessage(ChatColor.GRAY+"Command usage: /tokens give <player name> <tokens amount>");
                         }
                     }else{
                         commandSender.sendMessage(ChatColor.GRAY+"You don't have "+ChatColor.GOLD+""+num+""+ChatColor.GRAY+" tokens.");
                     }
                 }else{
                     commandSender.sendMessage(ChatColor.RED+"Invalid command use! Your arguments were "+ Arrays.toString(args));
-                    commandSender.sendMessage(ChatColor.RED+"Try /tokens give <UserName> <Amount>");
+                    commandSender.sendMessage(ChatColor.GRAY+"Command usage: /tokens give <player name> <tokens amount>");
                 }
                 return true;
             }else{
@@ -83,19 +122,27 @@ public class CommandTokens implements CommandExecutor {
             }
         }else if(args.length==1) {
             Player target = Bukkit.getPlayer(args[0]);
-            if(commandSender.hasPermission("tokens.others")) {
-                if (target != null) {
-                    commandSender.sendMessage(args[0] + " has " + plugin.handler.getTokens(target) + " tokens.");
+            if(commandSender instanceof Player) {
+                if (commandSender.hasPermission("tokens.others")) {
+                    if (target != null) {
+                        commandSender.sendMessage(args[0] + " has " + plugin.handler.getTokens(target) + " tokens.");
+                    } else {
+                        commandSender.sendMessage("Couldn't find a player with the name " + args[0]);
+                    }
+                    return true;
                 } else {
+                    return false;// Player without tokens.others perms ran the command
+                }
+            }else{
+                if (target!=null){
+                    commandSender.sendMessage(args[0] + " has " + plugin.handler.getTokens(target) + " tokens.");
+                }else{
                     commandSender.sendMessage("Couldn't find a player with the name " + args[0]);
                 }
                 return true;
-            }else{
-                return false;// Player without tokens.others perms ran the command
             }
         }else{
            return false;// Server used /tokens command
-            //TODO: let server peep others token count
         }
     }
 }
