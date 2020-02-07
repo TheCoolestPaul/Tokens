33ca611517d867a6878e5916aee554ee730bf62a
TheCoolestPaul <Honda15251526>
2020-02-07 04:17:24 -0600

    Starting Cool-downs

--- src/main/java/net/thirdshift/tokens/keys/Key.java
+++ src/main/java/net/thirdshift/tokens/keys/Key.java
@@ -1,5 +1,12 @@
 package net.thirdshift.tokens.keys;
 
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Key {
 
     public String keyString;
@@ -7,9 +14,11 @@ public class Key {
     public boolean oneTime;
     public int tokens;
     public double cooldown;
+    public Map<Player, Long> cooldowns;
 
     public Key(String keyString){
         this.keyString=keyString;
+        this.cooldowns=new HashMap<>();
     }
 
     public Key(String keyString, boolean enabled, boolean oneTime, int tokens, double cooldown){
@@ -18,6 +27,7 @@ public class Key {
         this.oneTime=oneTime;
         this.tokens=tokens;
         this.cooldown=cooldown;
+        this.cooldowns=new HashMap<>();
     }
 
     public void setEnabled(boolean enabled) {
@@ -34,6 +44,18 @@ public class Key {
 
     public void setCooldown(double cooldown) {
         this.cooldown = cooldown;
+    }
+
+    public void setPlayerCooldown(Player player, long time){
+        if(time < 1){
+            cooldowns.remove(player);
+        }else{
+            cooldowns.put(player, time);
+        }
+    }
+
+    public long getCooldown(Player player){
+        return cooldowns.getOrDefault(player, (long) 0);
     }
 
     @Override
