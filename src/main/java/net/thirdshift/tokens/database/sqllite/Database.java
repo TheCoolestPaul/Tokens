b909fe0d6df7350b8b3783d6923dfeaaeae0cd45
RoyalBlueRanger <665978+rbluer@users.noreply.github.com>
2020-12-14 22:41:35 -0500

    Major bug fixes: Eliminated memory leaks related to not closing some open resources properly. Eliminated excessive overhead in adding a token to a player: The old process would perform 4 file system checks to see if directories and files existed, then it would perform a read from the database prior to turning around to perform a REPLACE of the player's record, of which the REPLACE performs a delete record transaction and then performs an insert transaction to finally add the player's data back to the database.  Final results were 3 database transactions and 4 file system transactions, plus forcing the rebuilding of the primary key index when the player was removed, and then added back.  The new process simply performs one database transaction: an update which does not force a primary key index rebuild. These optimizations also ensure all opened resources are properly closed under all conditions. A new function was added: addTokens.  This adds a number of tokens to the specified players which greatly reduces the overhead involved since no prior reads are required. Both MySQL and SQLite were optimized to improve performance and stability. Both utilize try with resources to ensure all closable resources are properly closed under all circumstances. This code was needed because of a situation where one player was generating over 150 tokens per minute and Token's old database code was heavily lagging the server.  Ideally, upwards of 200 tokens (individual transactions of 1 token per transaction) could be possible.  The goal is to support a few dozen players performing similar transactions, so this is the first step in to eliminating server lag from Tokens when there are very high demands from multiple players. Next phase is to add async caching to get database transactions out of bukkit's main sych thread to improve performance even more.

--- src/main/java/net/thirdshift/tokens/database/sqllite/Database.java
+++ src/main/java/net/thirdshift/tokens/database/sqllite/Database.java
@@ -1,77 +1,127 @@
 package net.thirdshift.tokens.database.sqllite;
 
-import net.thirdshift.tokens.Tokens;
-import org.bukkit.entity.Player;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 
+import org.bukkit.entity.Player;
+
+import net.thirdshift.tokens.Tokens;
+
 
 public abstract class Database {
-    Tokens plugin;
-    Connection connection;
+    private Tokens plugin;
+    private Connection connection;
 
     private final String table = "tokens_table";
-    public Database(Tokens instance){
+    public Database(Tokens instance) {
         plugin = instance;
     }
 
-    public abstract Connection getSQLConnection();
+    protected Tokens getPlugin() {
+    	return plugin;
+    }
+    
+    /**
+     * <p>This establishes the SQLite connection if it does not exist,
+     * and then upon subsequent calls, returns that connection. 
+     * A simple check with low overhead (if connection is null or closed) 
+     * is performed to establish if the more expensive checks and operations
+     * are required (file checks and establishing database connections).
+     * </p>
+     * 
+     */
+    public Connection getSQLConnection() {
+    	try {
+			if ( connection == null || connection.isClosed() ) {
+				// Once the connection is established, then skip all of this:
+				openConnection();
+			}
+		}
+		catch ( SQLException e ) {
+			plugin.getLogger().log( Level.SEVERE,
+            		String.format("Database ERROR: cannot establish if the connection is closed: %s", 
+            				e.getMessage()), e);
+		}
 
+    	return connection;
+    };
+    
+    protected void setConnection( Connection connection ) {
+    	this.connection = connection;
+    }
+
+    protected abstract void openConnection();
+    
     public abstract void load();
 
     public Integer getTokens(Player player) {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs;
-        try {
-            conn = getSQLConnection();
-            ps = conn.prepareStatement("SELECT * FROM " + table + " WHERE player = ?;");
+    	Integer results = 0;
+    	
+    	String sql = String.format( "SELECT * FROM %s WHERE player = ?;", table );
+        try (   Connection conn = getSQLConnection();
+        		PreparedStatement ps = conn.prepareStatement( sql );
+        		) {
             ps.setString(1, player.getUniqueId().toString());
-            rs = ps.executeQuery();
-            while(rs.next()){
-                if(rs.getString("player").equalsIgnoreCase(player.getUniqueId().toString())){
-                    return rs.getInt("tokens");
-                }
+            
+            try ( ResultSet rs = ps.executeQuery(); ) {
+            	
+            	while(rs.next()){
+            		// If the query was selected by player's uuid, not sure why it has to be retested here:
+            		if(rs.getString("player").equalsIgnoreCase(player.getUniqueId().toString())){
+            			results = rs.getInt("tokens");
+            		}
+            	}
             }
+            
         } catch (SQLException ex) {
             plugin.getLogger().log(Level.SEVERE, Errors.sqlConnectionExecute(), ex);
-        } finally {
-            try {
-                if (ps != null)
-                    ps.close();
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException ex) {
-                plugin.getLogger().log(Level.SEVERE, Errors.sqlConnectionClose(), ex);
-            }
         }
-        return 0;
+        return results;
     }
 
     public void setTokens(Player player, Integer tokens) {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        try {
-            conn = getSQLConnection();
-            ps = conn.prepareStatement("REPLACE INTO " + table + " (player,tokens) VALUES(?,?)");
+    	String sql = String.format( "REPLACE INTO %s (player,tokens) VALUES(?,?)", table );
+        try (   Connection conn = getSQLConnection();
+        		PreparedStatement ps = conn.prepareStatement( sql );
+        		) {
             ps.setString(1, player.getUniqueId().toString());
             ps.setInt(2, tokens);
+            
             ps.executeUpdate();
-        } catch (SQLException ex) {
+        } 
+        catch (SQLException ex) {
             plugin.getLogger().log(Level.SEVERE, Errors.sqlConnectionExecute(), ex);
-        } finally {
-            try {
-                if (ps != null)
-                    ps.close();
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException ex) {
-                plugin.getLogger().log(Level.SEVERE, Errors.sqlConnectionClose(), ex);
-            }
         }
+    }
+    
+    /**
+     * This function will add the given number of tokens to the player's balance without
+     * having to first perform a read from the database.  This will reduce overhead and
+     * improve performance.
+     * 
+     * This function uses try with resources to ensure all resources are properly closed.
+     * 
+     * @param player
+     * @param tokens
+     */
+    public int addTokens(Player player, Integer tokens) {
+    	int results = 0;
+    	String sql = String.format( "UPDATE %s set tokens = tokens + ? " +
+							" where player = ? ", table );
+    	try (   Connection conn = getSQLConnection();
+    			PreparedStatement ps = conn.prepareStatement( sql );
+    		) {
+    		ps.setInt(1, tokens);
+    		ps.setString(2, player.getUniqueId().toString());
+    		
+    		results = ps.executeUpdate();
+    	} 
+    	catch (SQLException ex) {
+    		plugin.getLogger().log(Level.SEVERE, Errors.sqlConnectionExecute(), ex);
+    	}
+    	return results;
     }
 }
