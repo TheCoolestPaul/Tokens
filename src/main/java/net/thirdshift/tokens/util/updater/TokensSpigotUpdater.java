cd733c1da0e165d067913652e1485820f9922b69
RoyalBlueRanger <665978+rbluer@users.noreply.github.com>
2020-12-14 22:07:50 -0500

    Fixed the semantic versions comparisons, of which was not working as per the standards as defined and published at semver.org.  Added semantic version tools to handle the comparisons, along with junit tests.  This was required since the existing code could not properly handle the use of valid patch and prerelease versioning. Fixed a rare memory leak when loading the version from the api.spigotmc.org; if there was a failure with closing the inputstream, then the bufferedReader would not be closed. Also the bufferedReader should have been closed first (last opened, first closed). Using try with resources to ensure all connections are properly closed.

--- src/main/java/net/thirdshift/tokens/util/updater/TokensSpigotUpdater.java
+++ src/main/java/net/thirdshift/tokens/util/updater/TokensSpigotUpdater.java
@@ -1,174 +1,220 @@
 package net.thirdshift.tokens.util.updater;
 
-import net.thirdshift.tokens.Tokens;
-import net.thirdshift.tokens.util.TokensEventListener;
-import org.apache.commons.lang.StringUtils;
-import org.bukkit.event.EventException;
-import org.bukkit.event.EventPriority;
-import org.bukkit.plugin.EventExecutor;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class TokensSpigotUpdater {
+import net.thirdshift.tokens.Tokens;
+import net.thirdshift.tokens.semver.BluesSemanticVersionData;
+import net.thirdshift.tokens.util.TokensEventListener;
 
-    private final int project;
-    private URL checkURL;
+public class TokensSpigotUpdater {
+	
+	public static final String SPIGOT_API_URL = "https://api.spigotmc.org/legacy/update.php?resource=";
+	public static final String SPIGOT_RESOURCE_URL = "https://www.spigotmc.org/resources/";
+
+	private final Tokens plugin;
+    private final int projectID;
+
+    private URL spigotCheckURL;
     private String newVersion;
-    private final JavaPlugin plugin;
-    private TokensEventListener tokensEventListener;
 
     public TokensSpigotUpdater(Tokens tokens, int projectID) {
-        this.plugin = tokens;
-        tokensEventListener=tokens.getTokensEventListener();
-        this.newVersion = plugin.getDescription().getVersion();
-        this.project = projectID;
-        try {
-            this.checkURL = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + projectID);
-        } catch (MalformedURLException e) {
-            plugin.getLogger().warning("Something went wrong checking for update");
-        }
+
+    	this.plugin = tokens;
+        this.projectID = projectID;
     }
 
-    public JavaPlugin getPlugin() {
+    public Tokens getPlugin() {
         return plugin;
     }
 
     public String getLatestVersion() {
         return newVersion;
     }
+    public void setLatestVersion( String newVersion ) {
+    	this.newVersion = newVersion;
+    }
 
-    public String getResourceURL() {
-        return "https://www.spigotmc.org/resources/" + project;
+    public int getProjectID() {
+		return projectID;
+	}
+
+	public URL getSpigotCheckURL() {
+		return spigotCheckURL;
+	}
+	public void setSpigotCheckURL( URL spigotCheckURL ) {
+		this.spigotCheckURL = spigotCheckURL;
+	}
+
+	public String getResourceURL() {
+        return SPIGOT_RESOURCE_URL + getProjectID();
     }
 
     public boolean checkForUpdates() throws Exception {
-        URLConnection con = checkURL.openConnection();
-        InputStreamReader streamReader = null;
-        BufferedReader buff = null;
+    	
         try {
-            streamReader = new InputStreamReader(con.getInputStream());
-            buff = new BufferedReader(streamReader);
-            this.newVersion = buff.readLine();
-        }finally{
-            if(streamReader!=null)
-                streamReader.close();
-            if(buff!=null)
-                buff.close();
+        	setSpigotCheckURL( new URL(SPIGOT_API_URL + getProjectID()) );
+        } 
+        catch (MalformedURLException e) {
+            getPlugin().getLogger().warning("TokensSpigotUpdater.checkForUpdates: " + 
+            			"Failed to construct the URL to api.spigotmc.org. " +
+            			"Something went wrong checking for update");
+            return true;
         }
-        String insVer = plugin.getDescription().getVersion();
-        int compare = VersionCompare(insVer, newVersion);
-        if(compare==0) {
+    	
+        URLConnection con = getSpigotCheckURL().openConnection();
+        try (
+        		InputStreamReader streamReader = new InputStreamReader(con.getInputStream());
+        		BufferedReader buff = new BufferedReader(streamReader);
+        		) {
+        	
+        	setLatestVersion( buff.readLine() );
+        }
+        catch ( Exception e ) {
+        	getPlugin().getLogger().info("TokensSpigotUpdater.checkForUpdates: " +
+        			"Failed to load data from api.spigotmc.org. [" + e.getMessage() + "]");
+
+        	return true;
+        }
+
+        String currentVersion = getPlugin().getDescription().getVersion();
+        
+        BluesSemanticVersionData svCurrentVersion = new BluesSemanticVersionData(currentVersion);
+        if ( !svCurrentVersion.isValid() ) {
+        	getPlugin().getLogger().info("TokensSpigotUpdater.checkForUpdates: " +
+        			"Current version has an invalid semantic version. " +
+        			"It is unknow if a newer version is available.");
+            return true;
+        }
+        
+        BluesSemanticVersionData svAvailableVersion = new BluesSemanticVersionData(
+        											getLatestVersion() );
+        if ( !svCurrentVersion.isValid() ) {
+        	getPlugin().getLogger().info("TokensSpigotUpdater.checkForUpdates: " +
+        			"Available version has an invalid semantic version. It maybe a newer version.");
+        	return true;
+        }
+        
+        getPlugin().getLogger().info( "Checking for updates:" +
+        			"  Current Version: " + svCurrentVersion.toString() +
+        			"  Available Version: " + svAvailableVersion.toString() );
+
+        int compare = svCurrentVersion.compareTo( svAvailableVersion );
+        
+        if( compare == 0 ) {
+        	// Same version: 
+        	getPlugin().getLogger().info("Tokens is up todate." );
             return false;
-        }else if(compare==1){
-            plugin.getLogger().info("Pre-release version");
+        }
+        else if( compare > 0 ) {
+        	getPlugin().getLogger().info( "This is a Pre-release version of Tokens " +
+        					"(newer than what's available at spigotmc.org).");
             return false;
-        }else if(compare==2){
-            if ( !tokensEventListener.isOutdated() ){
-                tokensEventListener.setOutdated(true);
-                tokensEventListener.setUpdateURL(getResourceURL());
-                plugin.getLogger().info("We're notifying all operators of an available update on join.");
-            } else {
+        }
+        else {
+        	TokensEventListener teListener = getPlugin().getTokensEventListener();
+        	
+            if ( !teListener.isOutdated() ){
+            	teListener.setOutdated(true);
+            	teListener.setUpdateURL(getResourceURL());
+            	getPlugin().getLogger().info("We're notifying all operators of an available update on join.");
+            } 
+            else {
                 // I added this if-statement in case there are multiple updates while a server is live and hasn't updated.
-                if (!tokensEventListener.getUpdateURL().equals(getResourceURL())){
-                    tokensEventListener.setUpdateURL(getResourceURL());
+                if (!teListener.getUpdateURL().equals(getResourceURL())){
+                	teListener.setUpdateURL(getResourceURL());
                 }
-                plugin.getLogger().info("We're already notifying all operators of an available update on join.");
+                getPlugin().getLogger().info("We're already notifying all operators of an available update on join.");
             }
 
-            return true;
-        }else{
-            plugin.getLogger().info("An error occurred in the updater");
             return true;
         }
     }
 
-    /**
-     * Compares two version strings.
-     *
-     * Use this instead of String.compareTo() for a non-lexicographical
-     * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
-     *
-     * @param v1 a string of alpha numerals separated by decimal points.
-     * @param v2 a string of alpha numerals separated by decimal points.
-     * @return The result is 1 if v1 is greater than v2.
-     *         The result is 2 if v2 is greater than v1.
-     *         The result is -1 if the version format is unrecognized.
-     *         The result is zero if the strings are equal.
-     */
-    public int VersionCompare(String v1,String v2){
-        int v1Len= StringUtils.countMatches(v1,".");
-        int v2Len=StringUtils.countMatches(v2,".");
-
-        if(v1Len!=v2Len)
-        {
-            int count=Math.abs(v1Len-v2Len);
-            if(v1Len>v2Len) {
-                StringBuilder v2Builder = new StringBuilder(v2);
-                for(int i = 1; i<=count; i++)
-                    v2Builder.append(".0");
-                v2 = v2Builder.toString();
-            } else {
-                StringBuilder v1Builder = new StringBuilder(v1);
-                for(int i = 1; i<=count; i++)
-                    v1Builder.append(".0");
-                v1 = v1Builder.toString();
-            }
-        }
-
-        if(v1.equals(v2))
-            return 0;
-
-        String[] v1Str=StringUtils.split(v1, ".");
-        String[] v2Str=StringUtils.split(v2, ".");
-        for(int i=0;i<v1Str.length;i++)
-        {
-            StringBuilder str1= new StringBuilder();
-            StringBuilder str2= new StringBuilder();
-            for (char c : v1Str[i].toCharArray()) {
-                if(Character.isLetter(c))
-                {
-                    int u=c-'a'+1;
-                    if(u<10)
-                        str1.append("0").append(u);
-                    else
-                        str1.append(u);
-                }
-                else
-                    str1.append(c);
-            }
-            for (char c : v2Str[i].toCharArray()) {
-                if(Character.isLetter(c))
-                {
-                    int u=c-'a'+1;
-                    if(u<10)
-                        str2.append("0").append(u);
-                    else
-                        str2.append(u);
-                }
-                else
-                    str2.append(c);
-            }
-            v1Str[i]="1"+str1;
-            v2Str[i]="1"+str2;
-
-            int num1=Integer.parseInt(v1Str[i]);
-            int num2=Integer.parseInt(v2Str[i]);
-
-            if(num1!=num2)
-            {
-                if(num1>num2)
-                    return 1;
-                else
-                    return 2;
-            }
-        }
-        return -1;
-    }
+//    /**
+//     * Compares two version strings.
+//     *
+//     * Use this instead of String.compareTo() for a non-lexicographical
+//     * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
+//     *
+//     * @param v1 a string of alpha numerals separated by decimal points.
+//     * @param v2 a string of alpha numerals separated by decimal points.
+//     * @return The result is 1 if v1 is greater than v2.
+//     *         The result is 2 if v2 is greater than v1.
+//     *         The result is -1 if the version format is unrecognized.
+//     *         The result is zero if the strings are equal.
+//     */
+//    public int VersionCompare(String v1,String v2){
+//        int v1Len= StringUtils.countMatches(v1,".");
+//        int v2Len=StringUtils.countMatches(v2,".");
+//
+//        if(v1Len!=v2Len)
+//        {
+//            int count=Math.abs(v1Len-v2Len);
+//            if(v1Len>v2Len) {
+//                StringBuilder v2Builder = new StringBuilder(v2);
+//                for(int i = 1; i<=count; i++)
+//                    v2Builder.append(".0");
+//                v2 = v2Builder.toString();
+//            } else {
+//                StringBuilder v1Builder = new StringBuilder(v1);
+//                for(int i = 1; i<=count; i++)
+//                    v1Builder.append(".0");
+//                v1 = v1Builder.toString();
+//            }
+//        }
+//
+//        if(v1.equals(v2))
+//            return 0;
+//
+//        String[] v1Str=StringUtils.split(v1, ".");
+//        String[] v2Str=StringUtils.split(v2, ".");
+//        for(int i=0;i<v1Str.length;i++)
+//        {
+//            StringBuilder str1= new StringBuilder();
+//            StringBuilder str2= new StringBuilder();
+//            for (char c : v1Str[i].toCharArray()) {
+//                if(Character.isLetter(c))
+//                {
+//                    int u=c-'a'+1;
+//                    if(u<10)
+//                        str1.append("0").append(u);
+//                    else
+//                        str1.append(u);
+//                }
+//                else
+//                    str1.append(c);
+//            }
+//            for (char c : v2Str[i].toCharArray()) {
+//                if(Character.isLetter(c))
+//                {
+//                    int u=c-'a'+1;
+//                    if(u<10)
+//                        str2.append("0").append(u);
+//                    else
+//                        str2.append(u);
+//                }
+//                else
+//                    str2.append(c);
+//            }
+//            v1Str[i]="1"+str1;
+//            v2Str[i]="1"+str2;
+//
+//            int num1=Integer.parseInt(v1Str[i]);
+//            int num2=Integer.parseInt(v2Str[i]);
+//
+//            if(num1!=num2)
+//            {
+//                if(num1>num2)
+//                    return 1;
+//                else
+//                    return 2;
+//            }
+//        }
+//        return -1;
+//    }
 
 }
